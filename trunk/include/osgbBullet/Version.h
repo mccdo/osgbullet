c9445660838d6a2bc464517e76136ff1d16cfaef
SkewMatrix <SkewMatrix@52ec9183-9a46-0410-8497-359fc0c0ba5a>
2009-11-13 16:34:30 +0000

    Increment version number (now that we're past v1.0.0) to v1.0.1.

--- trunk/include/osgbBullet/Version.h
+++ trunk/include/osgbBullet/Version.h
@@ -30,7 +30,7 @@ namespace osgbBullet {
 
 #define OSGBBULLET_MAJOR_VERSION 1
 #define OSGBBULLET_MINOR_VERSION 0
-#define OSGBBULLET_SUB_VERSION 0
+#define OSGBBULLET_SUB_VERSION 1
 
 // C preprocessor integrated version number.
 // The form is Mmmss, where:
