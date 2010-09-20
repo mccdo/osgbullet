ecd53f5c1c8ef15c578a6615d2d67a720411a1d4
SkewMatrix <SkewMatrix@52ec9183-9a46-0410-8497-359fc0c0ba5a>
2010-09-20 21:43:04 +0000

    Removing mkdae scripts; modifying testosgb scripts to remove colladaread, eliminate old parameters for osgbpp, and display version information.

--- scripts/testosgb.sh
+++ scripts/testosgb.sh
@@ -14,34 +14,34 @@ echo
 echo Decimation tests.
 echo First, test the simpifier.
 echo
-osgbpp cow.osg -o out.dae --display --convexTM --overall
-osgbpp cow.osg -o out.dae --display --convexTM --overall --simplify .2
+osgbpp cow.osg --convexTM --overall
+osgbpp cow.osg --convexTM --overall --simplify .2
 echo
 echo Decimation tests.
 echo Test the decimator
 echo
-osgbpp cow.osg -o out.dae --display --convexTM --overall --decPercent .2
+osgbpp cow.osg --convexTM --overall --decPercent .2
 echo
 echo Decimation tests.
 echo Test the Vertex Aggregator.
 echo
-osgbpp cow.osg -o out.dae --display --convexTM --overall --aggMaxVerts 50
+osgbpp cow.osg --convexTM --overall --aggMaxVerts 50
 echo
 echo Decimation tests, this time with the pliers.
 echo First, test the simpifier.
 echo
-osgbpp pliers-big.osg -o out.dae --display --convexTM --overall
-osgbpp pliers-big.osg -o out.dae --display --convexTM --overall --simplify .2
+osgbpp pliers-big.osg --convexTM --overall
+osgbpp pliers-big.osg --convexTM --overall --simplify .2
 echo
 echo Decimation tests.
 echo Test the decimator
 echo
-osgbpp pliers-big.osg -o out.dae --display --convexTM --overall --decPercent .2
+osgbpp pliers-big.osg --convexTM --overall --decPercent .2
 echo
 echo Decimation tests.
 echo Test the Vertex Aggregator.
 echo
-osgbpp pliers-big.osg -o out.dae --display --convexTM --overall --aggMaxVerts 50
+osgbpp pliers-big.osg --convexTM --overall --aggMaxVerts 50
 
 
 echo
@@ -50,8 +50,8 @@ echo
 echo Testing the tetra.
 echo All should behave normally.
 echo
-osgbpp tetra.osg -o out.dae --display --convexHull
-osgbpp tetra.osg -o out.dae --display --convexTM
+osgbpp tetra.osg --convexHull
+osgbpp tetra.osg --convexTM
 
 echo
 echo
@@ -59,11 +59,11 @@ echo
 echo Testing the concave object.
 echo All should behave normally.
 echo
-osgbpp concave.osg -o out.dae --display --convexHull
-osgbpp concave.osg -o out.dae --display --convexHull --overall
-osgbpp concave.osg -o out.dae --display --triMesh
-osgbpp concave.osg -o out.dae --display --box
-osgbpp concave.osg -o out.dae --display --box --overall
+osgbpp concave.osg --convexHull
+osgbpp concave.osg --convexHull --overall
+osgbpp concave.osg --triMesh
+osgbpp concave.osg --box
+osgbpp concave.osg --box --overall
 
 echo
 echo
@@ -71,8 +71,8 @@ echo
 echo Testing the cow.
 echo All should behave normally.
 echo
-osgbpp cow.osg -o out.dae --display --convexHull --overall
-osgbpp cow.osg -o out.dae --display --triMesh --overall
+osgbpp cow.osg --convexHull --overall
+osgbpp cow.osg --triMesh --overall
 
 echo
 echo
@@ -81,32 +81,23 @@ echo Center of mass tests.
 echo First test \(cube\) should behave normally.
 echo Next four cubes should behave as if COM is outside lower left corner.
 echo
-osgbpp offcube.osg -o out.dae --display --box
-osgbpp offcube.osg -o out.dae --display --box --com 0,0,0
-osgbpp offcube.osg -o out.dae --display --triMesh --com 0,0,0
-osgbpp offcube.osg -o out.dae --display --convexTM --com 0,0,0
-osgbpp offcube.osg -o out.dae --display --convexHull --com 0,0,0
+osgbpp offcube.osg --box
+osgbpp offcube.osg --box --com 0,0,0
+osgbpp offcube.osg --triMesh --com 0,0,0
+osgbpp offcube.osg --convexTM --com 0,0,0
+osgbpp offcube.osg --convexHull --com 0,0,0
 echo
 echo Center of mass tests.
 echo Concave object should behave as if COM is outside to the right.
 echo Cow should behave as if COM is outside to the right-back.
 echo
-osgbpp concave.osg -o out.dae --display --box --com 5,0,0
-osgbpp cow.osg -o out.dae --display --convexHull --overall --com 2,0,0
+osgbpp concave.osg --box --com 5,0,0
+osgbpp cow.osg --convexHull --overall --com 2,0,0
 
 
 echo
 echo Decimation combined with center of mass.
 echo
-osgbpp cow.osg -o out.dae --display --convexTM --overall --simplify .2 --com 4,-.5,0
-osgbpp cow.osg -o out.dae --display --convexTM --overall --decPercent .2  --com 4,-.5,0
-osgbpp cow.osg -o out.dae --display --convexTM --overall --aggMaxVerts 50  --com 4,-.5,0
-
-
-echo
-echo
-echo
-echo Test reading collada files
-echo Valid keys are 0 and 1.
-echo
-colladaread tetra.osg
+osgbpp cow.osg --convexTM --overall --simplify .2 --com 4,-.5,0
+osgbpp cow.osg --convexTM --overall --decPercent .2  --com 4,-.5,0
+osgbpp cow.osg --convexTM --overall --aggMaxVerts 50  --com 4,-.5,0
