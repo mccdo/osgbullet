2d5e24f22add875adae09900c6965f90d9d55d0a
SkewMatrix <SkewMatrix@52ec9183-9a46-0410-8497-359fc0c0ba5a>
2011-09-10 23:44:06 +0000

    Copyright blocks, some code cleanup.

--- trunk/src/osgbInteraction/HandNode.cpp
+++ trunk/src/osgbInteraction/HandNode.cpp
@@ -1,4 +1,22 @@
-// Copyright (c) 2008-1009 Skew Matrix Software LLC. All rights reserved.
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * osgBullet is (C) Copyright 2009-2011 by Kenneth Mark Bryden
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
 
 #include <osgbInteraction/HandNode.h>
 #include <osgbInteraction/ArticulationRecord.h>
@@ -35,11 +53,13 @@ void dumpNP( const osg::NodePath& p )
     osg::notify( osg::ALWAYS ) << std::endl;
 }
 
+
 namespace osgbInteraction
 {
 
 
 
+/** \cond */
 // ScaleVisitor is designed specifically to scale the hand model.
 // The hand model must be scaled into an app-specified size at load time to
 //   avoid the complexities of scaling in Bullet. Bullet does not support
@@ -421,6 +441,7 @@ protected:
         return NULL;
     }
 };
+/** \endcond */
 
 
 
--- trunk/src/osgbInteraction/p5support.cpp
+++ trunk/src/osgbInteraction/p5support.cpp
@@ -1,3 +1,23 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * osgBullet is (C) Copyright 2009-2011 by Kenneth Mark Bryden
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
 // Copyright (c) 2010 AlphaPixel LLC. All rights reserved.
 // Inspired by code from TestDevP5.cpp
 // This code relies on the driver in A__DualModeDriverBeta31rd.zip, which includes the necessary
@@ -76,7 +96,7 @@ P5Glove::~P5Glove()
 } // P5Glove::~P5Glove
 
 // returns false to indicate error abort
-bool P5Glove::updateHandState(osgbInteraction::HandNode* hn)
+bool P5Glove::updateHandState( osgbInteraction::HandNode* hn )
 {
 	if(!_ready) return(false);
 
