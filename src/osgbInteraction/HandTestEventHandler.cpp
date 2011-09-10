ae02165ec542083322c92817749847338e48bb50
SkewMatrix <SkewMatrix@52ec9183-9a46-0410-8497-359fc0c0ba5a>
2011-09-10 23:44:06 +0000

    Copyright blocks, some code cleanup.

--- src/osgbInteraction/HandTestEventHandler.cpp
+++ src/osgbInteraction/HandTestEventHandler.cpp
@@ -1,11 +1,30 @@
-// Copyright (c) 2008-2010 Blue Newt Software LLC and Skew Matrix Software LLC. All rights reserved.
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * osgBullet is (C) Copyright 2009-2011 by Kenneth Mark Bryden
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
 
 #include <osgbInteraction/HandTestEventHandler.h>
 #include <osgbInteraction/HandNode.h>
 #include <osgwTools/Quat.h>
 
 
-namespace osgbInteraction {
+namespace osgbInteraction
+{
 
 
 HandTestEventHandler::HandTestEventHandler( osgbInteraction::HandNode* hn )
